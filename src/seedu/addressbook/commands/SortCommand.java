6c423c55f04765f51d2932abf8cc376b744f8d51
yash2198 <yashchowdhary98@gmail.com>
2018-02-04 15:04:06 +0800

    SortCommand.java: added logic

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,4 +1,32 @@
 package seedu.addressbook.commands;
 
-public class SortCommand {
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Sorts all the data in the address book in alphabetical order of Name
+ */
+public class SortCommand extends Command{
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts all persons in the address book in alphabetical order of their name.\n"
+            + "Example: " + COMMAND_WORD;
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        Collections.sort(allPersons, new Comparator<ReadOnlyPerson>() {
+            @Override
+            public int compare(ReadOnlyPerson o1, ReadOnlyPerson o2) {
+                return o1.getName().toString().compareTo(o2.getName().toString());
+            }
+        });
+
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+
+    }
 }
