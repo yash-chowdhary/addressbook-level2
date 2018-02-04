17046bdc516bc50aed1816768d764285083b4330
yash2198 <yashchowdhary98@gmail.com>
2018-02-04 15:39:27 +0800

    SortCommand.java: now calls helper sort() method in AddressBook.java

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,11 +1,5 @@
 package seedu.addressbook.commands;
 
-import seedu.addressbook.data.person.ReadOnlyPerson;
-
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 /**
  * Sorts all the data in the address book in alphabetical order of Name
  */
@@ -16,22 +10,14 @@ public class SortCommand extends Command{
             + ": Sorts all persons in the address book in alphabetical order of their name.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "All persons in address book sorted by Name";
-    public static final String MESSAGE_EMPTY_BOOK = "No persons in address book to sort.";
+    public static final String MESSAGE_SUCCESS = "All persons in address book sorted by name";
+    public static final String MESSAGE_FAILURE = "Unable to sort persons in address book.";
 
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        if(allPersons.isEmpty()){
-            return new CommandResult(MESSAGE_EMPTY_BOOK);
+        if(!addressBook.sort()){
+            return new CommandResult(MESSAGE_FAILURE);
         }
-        Collections.sort(allPersons, new Comparator<ReadOnlyPerson>() {
-            @Override
-            public int compare(ReadOnlyPerson o1, ReadOnlyPerson o2) {
-                return o1.getName().toString().compareTo(o2.getName().toString());
-            }
-        });
-
         return new CommandResult(MESSAGE_SUCCESS);
 
     }
