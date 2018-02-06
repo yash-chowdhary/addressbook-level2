27cd0a4d65d6eada747865fd8b84c6e01340157f
yash2198 <yashchowdhary98@gmail.com>
2018-02-06 17:37:12 +0800

    Exception-handling for read-only file.

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.commands.ExitCommand;
+import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.parser.Parser;
@@ -84,9 +85,13 @@ public class Main {
         do {
             String userCommandText = ui.getUserCommand();
             command = new Parser().parseCommand(userCommandText);
-            CommandResult result = executeCommand(command);
-            recordResult(result);
-            ui.showResultToUser(result);
+            try{
+                CommandResult result = executeCommand(command);
+                recordResult(result);
+                ui.showResultToUser(result);
+            } catch (StorageOperationException soe){
+                new Main().run(new String[]{});
+            }
 
         } while (!ExitCommand.isExit(command));
     }
@@ -105,13 +110,17 @@ public class Main {
      * @param command user command
      * @return result of the command
      */
-    private CommandResult executeCommand(Command command)  {
+    private CommandResult executeCommand(Command command) throws StorageOperationException {
         try {
             command.setData(addressBook, lastShownList);
             CommandResult result = command.execute();
             storage.save(addressBook);
             return result;
-        } catch (Exception e) {
+        } catch (StorageOperationException soe){
+            ui.showToUser(Messages.MESSAGE_READ_ONLY);
+            throw new StorageOperationException(Messages.MESSAGE_READ_ONLY);
+        }
+        catch (Exception e) {
             ui.showToUser(e.getMessage());
             throw new RuntimeException(e);
         }
