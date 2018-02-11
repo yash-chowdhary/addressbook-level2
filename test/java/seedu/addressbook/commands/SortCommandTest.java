f01fe40c2d52ef1125868a7f23f1e3283a70da6e
yash2198 <yashchowdhary98@gmail.com>
2018-02-11 12:52:05 +0800

    SortCommandTest.java: added test logic

--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -1,10 +1,37 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.util.TestUtil;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class SortCommandTest {
 
     public void sortCommand_addressBookWithFiveElements_sortedAddressBook(){
+        SortCommand sortCommand = new SortCommand();
+
+        Person miguel = TestUtil.generateTestPerson("Miguel");
+        Person suzanne = TestUtil.generateTestPerson("Suzanne");
+        Person carley = TestUtil.generateTestPerson("Carley");
+        Person genna = TestUtil.generateTestPerson("Genna");
+        Person huey = TestUtil.generateTestPerson("Huey");
+
+        AddressBook addressBook = TestUtil.createAddressBook(miguel,suzanne,carley,genna,huey);
+        List<Person> sortedList = Arrays.asList(carley,genna,huey,miguel,suzanne);
+
+        sortCommand.setData(addressBook, Collections.emptyList());
+        sortCommand.execute();
+
+        UniquePersonList personList = addressBook.getAllPersons();
+        UniquePersonList expectedList = new UniquePersonList(personList);
+
+        assertTrue(sortedList.equals(expectedList));
     }
 
 }
