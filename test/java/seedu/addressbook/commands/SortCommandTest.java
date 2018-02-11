40a8d21dfaea7d1e258d618892e45329da001f0c
yash2198 <yashchowdhary98@gmail.com>
2018-02-11 12:45:08 +0800

    SortCommandTest.java: added test class

---
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.commands;
+
+import static org.junit.Assert.*;
+
+public class SortCommandTest {
+
+    public void sortCommand_addressBookWithFiveElements_sortedAddressBook(){
+    }
+
+}
